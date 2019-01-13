4ac144502908776e372b7574f5a1ffa9bc3286db
Efrat Traube <efratraube@gmail.com>
2019-01-13 08:47:49 -0800

    models first iteration

--- facts/models.py
+++ facts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Artist(models.Model):
+    full_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.full_name
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Fact(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    text = models.TextField(blank=True, null=True)
+    publisher_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.text
