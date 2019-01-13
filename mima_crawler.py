b1584a828772a3cd6b84e06899ff0881fe318fd4
Efrat <efratraube@gmail.com>
2019-01-13 23:28:09 +0200

    final crawler ready

--- mima_crawler.py
+++ mima_crawler.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from facts import models
 
 # artist_collection = set()
 # page_link = "https://www.mima.co.il/artist_page.php?artist_id=19"
@@ -24,36 +25,76 @@ import re
 #     print(a['href'])
 
 
-def get_artists_by_letters_pages(main_page_link,letter_page_name):
-    # page_link = "https://www.mima.co.il/"
-    page_response = requests.get(main_page_link, timeout=5)
-    page_content = BeautifulSoup(page_response.content, "html.parser")
+# def get_artists_by_letters_pages(main_page_link,letter_page_name):
+#     # page_link = "https://www.mima.co.il/"
+#     page_response = requests.get(main_page_link, timeout=5)
+#     page_content = BeautifulSoup(page_response.content, "html.parser")
+#
+#     letters = page_content.find_all("a", attrs={"href": re.compile(rf'^{letter_page_name}')})
+#     for let in letters:
+#         # print(a.text)
+#         # print(a['href'])
+#         letters_pages.add(let['href'])
 
-    letters = page_content.find_all("a", attrs={"href": re.compile(rf'^{letter_page_name}')})
-    for let in letters:
-        # print(a.text)
-        # print(a['href'])
-        letters_pages.add(let['href'])
-
-def get_artist_from_letter_page(letter_link,artist_page_name):
-    page_response = requests.get(letter_link, timeout=5)
-    page_content = BeautifulSoup(page_response.content, "html.parser")
-
-    artists = page_content.find_all("a",attrs={"href":re.compile(rf'^{artist_page_name}')})
-    for a in artists:
-        # print(a.text)
-        artists_names.add(a.text)
-        artists_pages[re.search('=([0-9]*)',a['href']).group(1)] = a['href']
-        # artists_pages.add(,(a['href']))
+# def get_artist_from_letter_page(letter_link,artist_page_name):
+#     page_response = requests.get(letter_link, timeout=5)
+#     page_content = BeautifulSoup(page_response.content, "html.parser")
+#
+#     artists = page_content.find_all("a",attrs={"href":re.compile(rf'^{artist_page_name}')})
+#     for a in artists:
+#         # print(a.text)
+#         artists_names.add(a.text)
+#         artists_pages[re.search('=([0-9]*)',a['href']).group(1)] = a['href']
+#         # artists_pages.add(,(a['href']))
         # print(a['href'])
         # print(re.search('=([0-9]*)',a['href']).group(1))
 
+# def get_sub_pages_from_link(link,sub_page_name):
+#     info = []
+#     page_response = requests.get(link, timeout=5)
+#     page_content = BeautifulSoup(page_response.content, "html.parser")
+#     pages = page_content.find_all("a", attrs={"href": re.compile(rf'^{sub_page_name}')})
+#     for i,page in enumerate(pages):
+#         info.append({
+#             'name':page.text,
+#             'link':page['href'],
+#             'get_param': re.search('=([0-9\W]*)', page['href']).group(1)
+#         })
+#         # print(page)
+#     return info
+
+re_param = re.compile('=([0-9\W]*)')
+
+def get_sub_pages_from_link(link,re_sub_page_name):
+    info = []
+    page_response = requests.get(link, timeout=5)
+    page_content = BeautifulSoup(page_response.content, "html.parser")
+    pages = page_content.find_all("a", attrs={"href": re_sub_page_name})
+    for page in pages:
+        info.append({
+            'name':page.text,
+            'link':page['href'],
+            # 'get_param': re_param.match(page['href']).group(1)
+        })
+        # print(page)
+    return info
+
+
 site_url = "https://www.mima.co.il/"
-letters_pages = set()
-artists_pages = dict()
-artists_names = set()
-get_artists_by_letters_pages(site_url,"artist_letter")
-for let in letters_pages:
-    get_artist_from_letter_page(site_url + let,"artist_page")
-print(len(artists_pages))
-print(len(artists_names))
+
+letters_pages = get_sub_pages_from_link(site_url,re.compile(rf'^artist_letter'))
+print(len(letters_pages))
+artist_pages = []
+artist_re = re.compile(rf'^artist_page')
+for letter in letters_pages:
+    artist_pages.extend(get_sub_pages_from_link(site_url + letter['link'],artist_re))
+print(len(artist_pages))
+songs_pages = []
+song_re = re.compile(rf'^fact_page')
+i = 0
+for artist in artist_pages:
+    # print(i)
+    # i+=1
+    artist['songs_list'] = get_sub_pages_from_link(site_url + artist['link'],song_re)
+    # songs_pages.extend(get_sub_pages_from_link(site_url + artist['link'],song_re))
+print(len(songs_pages))
